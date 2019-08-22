281247613e602c2dd37ff1cb04dc6e9b1450cdde
Roberto Samia <robertosamia@Robertos-Mini.attlocal.net>
2019-08-22 19:23:38 -0400

    corrected route error for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json }, path: '/' do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: [:show]
     end
