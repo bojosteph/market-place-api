bd1486c22383a08a9a161c189546126317160d77
Roberto Samia <robertosamia@Robertos-Mini.attlocal.net>
2019-08-22 18:26:38 -0400

    Create user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json } do
-    namespace :v1 do
+  namespace :api, defaults: { format: :json }, path: '/' do
+    scope module: :v1 do
 
+    end
   end
 end
