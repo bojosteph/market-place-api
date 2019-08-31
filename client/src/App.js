7407dab4fb4da7506a8dfd24b26152544896d127
Roberto Samia <bojosteph@gmail.com>
2019-08-31 08:40:02 -0400

    added redux store

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import NavBar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
@@ -10,14 +13,16 @@ import Login from "./components/auth/Login";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <NavBar />
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-      </div>
-    </Router>
+  <Provider store={store}>   
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
+  </Provider>
   );
 }
 
