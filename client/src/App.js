51db906bd881562a615198d1babcd96e244eddc0
Roberto Samia <bojosteph@gmail.com>
2019-08-30 23:15:45 -0400

    added navbar landing register and login component

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';    
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import NavBar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
